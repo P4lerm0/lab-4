b8a518a3adbebfe30d82bc9648db4d140f50a311
P4lerm0 <90239535+P4lerm0@users.noreply.github.com>
2021-11-10 00:42:55 +0300

    Add files via upload

---
+++ lab 4 part 3/lab 4 part 3/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Melyakina.L4.N3
+{
+    class Program
+    {
+
+        static double Method(double a)
+        {
+            double s = (a + 1) / 15;
+            return s;
+        }
+        static double Rec(double k)
+        {
+
+
+            if (k == -1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Method(k) * Rec(k - 1);
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            double s = Rec(n);
+
+
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ lab 4 part 3/lab 4 part 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab_4_part_3
+{
+    class Program
+    {
+        static double Method(double x)
+        {
+            double a = (-1 + x) / 2;
+                return a;
+        }
+        static double Rec(double k)
+        {
+            if (k == -1)
+                return 1;
+            else
+                return Method(k) * Rec(k - 1);
+
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            double s = Rec(n);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
